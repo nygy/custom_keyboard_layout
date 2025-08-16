6210fd91bd9e4d278e6ab6cd2ec99b01e4eeb73e
nygy <gyuri.nyitrai@gmail.com>
2025-08-16 18:44:17 +0200

    formatting

--- sticker generator.py
+++ sticker generator.py
@@ -3,8 +3,8 @@ import ast
 from svgwrite import Drawing
 
 with open('custom layout.txt', 'r') as file:
-     content = file.read()
-layout_def = ast.literal_eval(content)
+    file_content = file.read()
+layout_def = ast.literal_eval(file_content)
 
 dwg: Drawing = svgwrite.Drawing('pattern.svg', size=("297mm", "210mm"), profile='tiny')
 sticker_radius = 6
@@ -13,7 +13,7 @@ button_gap = 3.25
 font_sizes = [22, 14, 14]
 x_offsets = [0, 0, 2.5]
 y_offsets = [0.5, 4, 4]
-button_text = ["Ű", "", "{"]
+
 
 def draw_button(cen_x: float, cen_y: float, content: list) -> None:
     dwg.add(dwg.circle(center=(f'{cen_x}mm', f'{cen_y}mm'),
@@ -24,7 +24,7 @@ def draw_button(cen_x: float, cen_y: float, content: list) -> None:
                      size=(f'{button_size-0.5}mm', f'{button_size-0.5}mm'),
                      stroke='red', stroke_width='1mm', stroke_opacity='0.5'))
     for i, char in enumerate(content):
-        if char.isalpha() & i == 1 :
+        if char.isalpha() & i == 1:
             continue
         dwg.add(dwg.text(content[i],
                          insert=(f'{cen_x+x_offsets[i]}mm', f'{cen_y+y_offsets[i]}mm'),
@@ -42,8 +42,8 @@ for row in rows:
     row_buttons = [sublist for sublist in layout_def if sublist[0] == row]
     row_len = len(row_buttons)
     print(f'{row=} {row_len=} {row_buttons=}')
-    for i in range(row_len):
-        pos_x = i*(button_size + button_gap - 0.4) + 20
+    for button_position in range(row_len):
+        pos_x = button_position*(button_size + button_gap - 0.4) + 20
         pos_y = 22*row
         button_texts = [txt for txt in layout_def[button_index][3:] if isinstance(txt, str)]
 #        print(f'{i=} {button_index=} {button_texts=} {pos_x=:.2f} {pos_y=:.2f}')
@@ -51,9 +51,3 @@ for row in rows:
         button_index = button_index + 1
 
 dwg.save()
-
-#char = 'ű'
-#code = ord(char)
-#hex_code = f"U+{code:04X}"
-#print(hex_code)  # Output: U+0171
-
